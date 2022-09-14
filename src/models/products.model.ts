358905485401d1a71efd5c5ad1a1fa1c70154d42
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-14 09:01:56 -0400

    Feat: Adiciona create em products na camada model para cadastrar produtos

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,6 +1,6 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
-import { IProduct } from '../interfaces/products.interface';
+import { IProduct, INewProduct } from '../interfaces/products.interface';
 
 class ProductModel {
   private connection: Pool;
@@ -14,6 +14,18 @@ class ProductModel {
     const [products] = await this.connection.execute(query);
 
     return products as IProduct[];
+  }
+
+  public async create(product: INewProduct): Promise<IProduct> {
+    const { name, amount } = product;
+    const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(query, [name, amount]);
+
+    return {
+      id: insertId,
+      name,
+      amount,
+    };
   }
 }
 
