04b4dbba271c5e446877f8fdfc01654d188e462f
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-14 09:09:15 -0400

    Feat: Adiciona as rotas e adiciona a middleware de validação na rota de cadastro

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRouter from './user.route';
+import productRouter from './products.route';
 
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('/products', productRouter);
 
 export default router;
--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ProductController from '../controllers/products.controller';
+import validateProduct from '../middlewares/product.middleware';
 
 const router = Router();
 
 const productController = new ProductController();
 
 router.get('/', productController.getAll.bind(productController));
+router.post('/', validateProduct, productController.create.bind(productController));
 
 export default router;
