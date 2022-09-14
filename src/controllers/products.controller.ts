01ef9e3c76b91cf8b76efa846503fff5eaef509d
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-14 09:04:48 -0400

    Feat: Adiciona o create a camada controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import ProductService from '../services/products.service';
-import { IProduct } from '../interfaces/products.interface';
+import { IProduct, INewProduct } from '../interfaces/products.interface';
 
 class ProductController {
   private service: ProductService;
@@ -14,6 +14,17 @@ class ProductController {
       const product: IProduct[] = await this.service.getAll();
 
       res.status(200).json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async create(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { name, amount }: INewProduct = req.body;
+      const product: IProduct = await this.service.create({ name, amount });
+
+      res.status(201).json(product);
     } catch (error) {
       next(error);
     }
