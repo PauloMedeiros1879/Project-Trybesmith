15dac92a3818e90f3cb929a2a84857a539b2341f
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-15 11:11:03 -0400

    Feat: Adiciona o login a camada service e atribui o arquivo unauthorized erro no login e o token REQ5

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 import UserModel from '../models/user.model';
-import { INewUser } from '../interfaces/user.interface';
+import { INewUser, IUserLogin } from '../interfaces/user.interface';
 import Token from '../helpers/token';
+import UnauthorizedError from '../errors/unauthorized.error';
 
 class UserService {
   private model: UserModel;
@@ -12,6 +13,18 @@ class UserService {
   public async create(user: INewUser): Promise<string> {
     const { username, id } = await this.model.create(user);
     const token = Token(username, id);
+
+    return token;
+  }
+
+  public async login(user: IUserLogin): Promise<string> {
+    const registeredUser = await this.model.login(user);
+
+    if (!registeredUser) {
+      throw new UnauthorizedError('Username or password invalid');
+    }
+
+    const token = Token(registeredUser.username, registeredUser.id);
 
     return token;
   }
