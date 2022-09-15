afd79f76fbb693f3eba8339ca35b096de937619f
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-15 10:08:06 -0400

    Feat: Adiciona o schema contendo o Joi de productsIds com a messagem solicitada REQ8

---
+++ src/schemas/orders.schemas.ts
@@ -0,0 +1,8 @@
+import Joi, { ObjectSchema } from 'joi';
+
+const orderSchema: ObjectSchema = Joi.object({
+  productsIds: Joi.array().items(Joi.number()).min(1).required()
+    .messages({ 'array.min': '"productsIds" must include only numbers' }),
+});
+
+export default orderSchema;
