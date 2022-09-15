dd24f6bff6083836a1d9c238f9692ebe40503447
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-15 10:14:53 -0400

    Feat: Adiciona a middleware de validação de orders nas rotas REQ8

--- src/routes/orders.route.ts
+++ src/routes/orders.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/orders.controller';
+import OrderValidate from '../middlewares/order.middleware';
 
 const router = Router();
 
 const ordersController = new OrdersController();
 
 router.get('/', ordersController.getAll.bind(ordersController));
-router.post('/', ordersController.create.bind(ordersController));
+router.post('/', OrderValidate, ordersController.create.bind(ordersController));
 
 export default router;
