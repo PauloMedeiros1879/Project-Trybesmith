72db14c8f8e9abc494d2de05b22957db357e2a9c
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-15 11:27:24 -0400

    Feat: Adiciona um arquivo login.route por que estava dando conflito colocar no use.route e altera o index.js de routes REQ5

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import userRouter from './user.route';
 import productRouter from './products.route';
+import loginRouter from './login.route';
 import orderRouter from './orders.route';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/products', productRouter);
+router.use('/login', loginRouter);
 router.use('/orders', orderRouter);
 
 export default router;
--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
-import validateUser from '../middlewares/use.middlewares';
 import validateLogin from '../middlewares/login.middleware';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/', validateUser, userController.create.bind(userController));
 router.post('/', validateLogin, userController.login.bind(userController));
 
 export default router;
