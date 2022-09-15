fd311a1b61e01c1cb07b98606984ac58092d288a
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-15 08:40:17 -0400

    Feat: Adiciona as rotas de orders

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRouter from './user.route';
 import productRouter from './products.route';
+import orderRouter from './orders.route';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/products', productRouter);
+router.use('/orders', orderRouter);
 
 export default router;
