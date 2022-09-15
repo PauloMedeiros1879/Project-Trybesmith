b9606b7680d62e0df30c714454186974a7de19cc
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-15 09:57:38 -0400

    Feat: Adiciona o endpoint cadastro de pedidos na camada controller REQ8

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import OrdersService from '../services/orders.service';
+import { ICustomRequest } from '../interfaces/standard.interface';
+import { IProductsOrder } from '../interfaces/orders.interface';
 
 class OrdersController {
   private service: OrdersService;
@@ -13,6 +15,21 @@ class OrdersController {
       const orders = await this.service.getAll();
 
       res.status(200).json(orders);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async create(req: ICustomRequest, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { productsIds }: IProductsOrder = req.body;
+
+      if (req.user) {
+        const { userId } = req.user;
+        const orders = await this.service.create(productsIds, userId);
+
+        res.status(201).json(orders);
+      }
     } catch (error) {
       next(error);
     }
